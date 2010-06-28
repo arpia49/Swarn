b9e9162e0070d3171d9f1db1f17b83351ae2bd6b
Adal Alom Rodríguez <arpia49@gmail.com>
2010-06-28 21:48:00 +0200

    Guarda el estado de la alarma entre sesiones.

--- src/com/arpia49/VistaAlarmas.java
+++ src/com/arpia49/VistaAlarmas.java
@@ -1,13 +1,16 @@
 package com.arpia49;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 public class VistaAlarmas extends Activity {
 
@@ -20,31 +23,75 @@ public class VistaAlarmas extends Activity {
 		cargarPosiciones((LinearLayout) findViewById(R.id.mainLay));
 
 		final CheckBox checkbox = (CheckBox) findViewById(1);
-
+		final CheckBox checkbox2 = (CheckBox) findViewById(2);
 		checkbox.setOnLongClickListener(new OnLongClickListener() {
 			public boolean onLongClick(View v) {
-				// Perform action on clicks, depending on whether it's now
-				// checked
-				if (((CheckBox) v).isChecked()) {
-					Toast.makeText(getApplicationContext(), "----ok",
-							Toast.LENGTH_SHORT).show();
-				} else {
-					Toast.makeText(getApplicationContext(), "ko----",
-							Toast.LENGTH_SHORT).show();
-				}
+				showDialog(1);
 				return true;
 			}
 		});
+
+		checkbox2.setOnLongClickListener(new OnLongClickListener() {
+			public boolean onLongClick(View v) {
+				showDialog(2);
+				return true;
+			}
+		});
+		
+		checkbox.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (((CheckBox) v).isChecked()) {
+					SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+					SharedPreferences.Editor editor = settings.edit();
+					editor.putBoolean("estadoAlarma1", true);
+					editor.commit();
+				} else {
+					SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+					SharedPreferences.Editor editor = settings.edit();
+					editor.putBoolean("estadoAlarma1", false);
+					editor.commit();
+				}
+			}
+		});
+
+		checkbox2.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (((CheckBox) v).isChecked()) {
+					SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+					SharedPreferences.Editor editor = settings.edit();
+					editor.putBoolean("estadoAlarma2", true);
+					editor.commit();
+				} else {
+					SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+					SharedPreferences.Editor editor = settings.edit();
+					editor.putBoolean("estadoAlarma2", false);
+					editor.commit();
+				}
+			}
+		});
+		
 	}
+
+	@Override
+	public Dialog onCreateDialog(int id) {
+		switch (id) {
+		default:
+			LayoutInflater li = LayoutInflater.from(this);
+			View opcionesAlarmas = li.inflate(R.layout.detalle_alarmas, null);
+			AlertDialog.Builder dialogoAlarmas = new AlertDialog.Builder(this);
+			dialogoAlarmas.setTitle("Opciones sobre la alarma");
+			dialogoAlarmas.setView(opcionesAlarmas);
+			return dialogoAlarmas.create();
+		}
+	}
+
 
 	private void meterBasuras() {
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 		SharedPreferences.Editor editor = settings.edit();
 		editor.putInt("numAlarmas", 2);
 		editor.putString("nombreAlarma1", "alarma a");
-		editor.putBoolean("estadoAlarma1", true);
 		editor.putString("nombreAlarma2", "alarma b");
-		editor.putBoolean("estadoAlarma2", false);
 		editor.commit();
 	}
 
