82f79e9e03c3d5aa3ceb56b2637d3a186672d2a8
Adal Alom Rodríguez <arpia49@gmail.com>
2010-09-21 15:58:45 +0200

    Entrada informativa en la lista de editar, que ya no muestra opciones para borrar todas las alarmas desde el menú.

--- src/com/arpia49/VistaAlarmasEditar.java
+++ src/com/arpia49/VistaAlarmasEditar.java
@@ -3,16 +3,15 @@ package com.arpia49;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class VistaAlarmasEditar extends ListActivity {
-
-	static final private int DEL_ALARMAS = Menu.FIRST;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -20,8 +19,17 @@ public class VistaAlarmasEditar extends ListActivity {
 		int numAlarmas = ListaAlarmas.size();
 		String[] alarmas = new String[numAlarmas];
 		for (int i = 1; i <= numAlarmas; i++) {
-			alarmas[i-1] = ListaAlarmas.element(i).getNombre();
+			alarmas[i - 1] = ListaAlarmas.element(i).getNombre();
 		}
+
+		TextView tv = new TextView(this);
+		tv.setId(-1);
+		tv.setText(getString(R.string.ayudaEditar));
+		tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f);
+		tv.setTypeface(Typeface.DEFAULT, 2);
+		ListView lv = getListView();
+		lv.addHeaderView(tv);
+
 		setListAdapter(new ArrayAdapter<String>(this, R.layout.lista_con_texto,
 				alarmas));
 	}
@@ -29,34 +37,11 @@ public class VistaAlarmasEditar extends ListActivity {
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-
-		Intent outData = new Intent();
-		outData.putExtra("idAlarma", (int)id);
-		setResult(Activity.RESULT_OK, outData);
-		finish();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		// Create and add new menu items.
-		MenuItem itemDel = menu.add(0, DEL_ALARMAS, Menu.NONE,
-				R.string.menu_del_todas_alarmas);
-		// Assign icons
-		itemDel.setIcon(R.drawable.del);
-		return true;
-	}
-
-	public boolean onOptionsItemSelected(MenuItem item) {
-		super.onOptionsItemSelected(item);
-		switch (item.getItemId()) {
-		case (DEL_ALARMAS): {
+		if (id > -1) {
 			Intent outData = new Intent();
-			outData.putExtra("todas", true);
+			outData.putExtra("idAlarma", (int) id);
 			setResult(Activity.RESULT_OK, outData);
 			finish();
 		}
-		}
-		return false;
 	}
 }
